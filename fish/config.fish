2732eb82113d482d3e2d6ed85534da37bc1962a6
abhix079 <abhishek788k@gmail.com>
2025-02-27 14:55:17 +0530

    fish

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias vi='nvim'
+    alias tmux='tmux -u'
+    alias fzfprev=' fzf --preview="bat --color=always {}" '
+    alias ff='fastfetch'
+    #alias ls='exa -l --icons'
+    alias clean='./cleanup.sh'
+    alias btop ='btop --utf-force'
+
+    set -x PATH /usr/bin/go $PATH
+end
+
+#function fish_prompt
+#    echo -n (hostname)' '(basename (pwd))' \$'
+#end
